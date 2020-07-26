74556f2bc576276d4686db0cab3f54dee9d01407
oka_shogo <cocorusakana@gmail.com>
2020-07-27 00:02:23 +0900

    firebaseの導入

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,55 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import "firebase/auth";
+import "firebase/firebase-firestore";
+
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvvQqBys0iT9wfbrW5qYkIaQwIr6Rb1fQ",
+  authDomain: "my-project-2020d.firebaseapp.com",
+  databaseURL: "https://my-project-2020d.firebaseio.com",
+  projectId: "my-project-2020d",
+  storageBucket: "my-project-2020d.appspot.com",
+  messagingSenderId: "933778217827",
+  appId: "1:933778217827:web:879ba61242dcd706d81156",
+  measurementId: "G-VBG8Z4EE82"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+class Root extends React.Component {
+  login(){
+    console.log("login");
+    auth.signInAnonymously();
+
+  }
+
+  logout(){
+    console.log("logout");
+    auth.signInAnonymously();
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <button onClick={this.login}>login</button>
+          <button onClick={this.logout}>logout</button>
+        </div>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </div>
+    );
+  }
+}
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Root />,
   document.getElementById('root')
 );
 
